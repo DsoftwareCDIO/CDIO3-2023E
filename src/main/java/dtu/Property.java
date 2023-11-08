a585123dce85f99826dbef75c036be003895784c
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-11-08 15:11:21 +0100

    Added Property

---
+++ src/main/java/dtu/Property.java
@@ -0,0 +1,33 @@
+package dtu;
+
+public class Property extends Field {
+    public enum Color {
+        ORANGE, YELLOW, RED, PURPLE, GREEN, CYAN, BLUE, BROWN
+    }
+
+    private Player owner;
+    private final int price;
+    private final Color color;
+
+    public Property(int price, Color color) {
+        super(Field.FieldType.PROPERTY);
+        this.price = price;
+        this.color = color;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
