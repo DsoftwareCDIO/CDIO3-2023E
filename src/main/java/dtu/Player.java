3cd96ec257bc47c8a9a250012a635a7dbc09ae5c
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-11-08 15:11:09 +0100

    Added player

---
+++ src/main/java/dtu/Player.java
@@ -0,0 +1,51 @@
+package dtu;
+
+public class Player {
+    public final Account account;
+    public final Piece piece;
+
+    private int getOutOfJailCards = 0;
+    private boolean uniqueCard = false;
+    private String name;
+
+    protected boolean inJail = false;
+
+    public Player(String name, int startCapital){
+        piece = new Piece();
+        account = new Account(startCapital);
+
+        this.name = name;
+    }
+
+    public void addGetOutOfJailCard() {
+        getOutOfJailCards++;
+    }
+
+    public void goToJail(){
+        inJail = true;
+        piece.setPosition(6);
+    }
+
+    public boolean getOutOfJail(){
+        if (getOutOfJailCards > 0) {
+            inJail = false;
+            getOutOfJailCards--;
+            return true;
+        }
+        else if (account.getMoney() > 0) {
+            inJail = false;
+            account.changeMoney(-1);
+            return true;
+        }
+        return false;
+    }
+
+    public void recieveUniqueCard() {
+        uniqueCard = true;
+    }
+
+    public boolean useUniqueCard(){
+        uniqueCard = false;
+        return uniqueCard;
+    }
+}
