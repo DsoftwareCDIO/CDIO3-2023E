3a149bbb7591f73c06c17f975d3e7cffefc5f30b
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-11-09 15:59:39 +0100

    Created ChanceCard

---
+++ src/main/java/dtu/ChanceCard.java
@@ -0,0 +1,14 @@
+package dtu;
+
+public class ChanceCard {
+    protected final int id;
+
+    public ChanceCard(int id) {
+        this.id = id;
+        //Activate things, description and so on is determined by id
+    }
+
+    public void activate(MonopolyJunior cont) {
+
+    }
+}
