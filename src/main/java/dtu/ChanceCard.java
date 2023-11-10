0b0f28fe64b8abe2838f5642e1adb162d9cdfa55
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-11-10 13:50:39 +0100

    Changed order of chancecards according to effect

--- src/main/java/dtu/ChanceCard.java
+++ src/main/java/dtu/ChanceCard.java
@@ -13,91 +13,91 @@ public class ChanceCard {
         switch (id) {
             case 0:
                 // effect
-                // car gets to choose what field to land on next round
-                break;
-            case 1:
-                // effect
-                // go to start
-                break;
-            case 2:
-                // effect
                 // go 5 fields ahead
-                break;
-            case 3:
-                // effect
-                // go to an orange field
-                // get it for free if its available
-                break;
-            case 4:
+                break;    
+            case 1:
                 // effect
                 // go one field ahead or take another chancecard
                 break;
-            case 5:
-                // effect
-                // ship gets to choose what field to land on next round
-                break;
-            case 6:
-                // effect
-                // pay 2 to the bank
-                break;
-            case 7:
-                // effect
-                // go to orange or green
-                // get it for free if its available
-                break;
-            case 8:
-                // effect
-                // go to lightblue field
-                // get it for free if its available
-                break;
-            case 9:
-                // effect
-                // get out of jail free card
-                break;
-            case 10:
-                // effect
-                // go to strandpromenaden
-                break;
-            case 11:
-                // effect
-                // cat gets to choose what field to land on next round
-                break;
-            case 12:
-                // effect
-                // dog gets to choose what field to land on next round
-                break;
-            case 13:
-                // effect
-                // get 1 from each player
-                break;
-            case 14:
-                // effect
-                // go to a pink or dark blue field
-                // get it for free if its available
-                break;
-            case 15:
+            case 2:
                 // effect
                 // get 2 from the bank
                 break;
-            case 16:
+            case 3:       
                 // effect
-                // go to a red feild
-                // you get it for free if its available
+                // pay 2 to the bank
                 break;
-            case 17:
+            case 4:    
+                // effect
+                // get 1 from each player
+                break;
+            case 5:    
+                // effect
+                // get out of jail free card
+                break;
+            case 6:
+                // effect
+                // go to start
+                break;
+            case 7:
+                // effect
+                // go to strandpromenaden
+                break;
+            case 8:
                 // effect
                 // go to the skatepark
                 // get it for free if its available
                 break;
-            case 18:
+            case 9:    
+                // effect
+                // go to an orange field
+                // get it for free if its available
+                break;
+            case 10:
+                // effect
+                // go to lightblue field
+                // get it for free if its available
+                break;
+            case 11:
+                // effect
+                // go to a red feild
+                // you get it for free if its available
+                break;
+            case 12:    
                 // effect
                 // go to a light blue or red field
                 // get it for free if its available
                 break;
-            case 19:
+            case 13:
                 // effect
                 // go to a brown or yellow field
                 // get it for free if its available
+                break;
+            case 14:
+                // effect
+                // go to orange or green
+                // get it for free if its available
+                break;
+            case 15:    
+                // effect
+                // go to a pink or dark blue field
+                // get it for free if its available
+                break;       
+            case 16:        
+                // effect
+                // ship gets to choose what field to land on next round
+                break;
+            case 17:
+                // effect
+                // cat gets to choose what field to land on next round
+                break;
+            case 18:
+                // effect
+                // dog gets to choose what field to land on next round
+                break;
+            case 19:
+                // effect
+                // car gets to choose what field to land on next round
                 break;
         }
     }
