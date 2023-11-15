9994c94e7ec208f5ecd4f8d5ebadaeb71509ba9b
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-11-15 14:40:26 +0100

    Created UIController

---
+++ src/main/java/dtu/UIController.java
@@ -0,0 +1,57 @@
+package dtu;
+
+public class UIController {
+    public static String[] drawMenu() {
+        return new String[]{};
+    }
+
+    public static void drawBoard(Player[] players) {
+
+    }
+
+    public static void movePlayer(int position, Player player) {
+
+    }
+
+    public static void updateFieldOwnership(int fieldId, Player player) {
+
+    }
+
+    public static void endGamePodium(Player[] players) {
+
+    }
+
+    public static void dieRollResult(int result) {
+
+    }
+
+    public static void updateMoneyInAccount(int money, Player player, boolean gained) {
+
+    }
+
+    public static void showNetWorth(Player player) {
+
+    }
+
+    public static void showChanceCard(ChanceCard card) {
+
+    }
+
+    public static void updateGetOutOfJailCards(int cards, boolean gained, Player player) {
+
+    }
+
+    public static void updateUniqueCards(boolean gained, Player player) {
+
+    }
+
+    public static int chooseFieldOnBoard(int[] fields) {
+        return 0;
+    }
+    
+    public static int chooseOption(String[] options){
+        return 1;
+    }
+
+    
+}
