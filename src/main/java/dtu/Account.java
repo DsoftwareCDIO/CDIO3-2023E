bbca1e6fcaf6516c27a207c2fe1f3c4ee4e3c894
TheSpicyOne <144691728+TheSpicyOne@users.noreply.github.com>
2023-11-15 11:08:17 +0100

    Added comments to account

--- src/main/java/dtu/Account.java
+++ src/main/java/dtu/Account.java
@@ -3,14 +3,17 @@ package dtu;
 public class Account {
     private int money;
 
+    //Constructor of account, only keeps track of money in account
     public Account(int startCapital){
         money = startCapital;
     }
 
+    //getMoney method
     public int getMoney() {
         return money;
     }
 
+    //Changemoney method, positive or negative
     public boolean changeMoney(int money) {
         if (this.money + money >= 0) {
             this.money += money;
