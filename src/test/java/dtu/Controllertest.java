3429448db21a0161a0ed44dd0fba473d4a2f0df8
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2023-11-22 11:35:31 +0100

    Fixed merge problem with controllertest

--- src/test/java/dtu/Controllertest.java
+++ src/test/java/dtu/Controllertest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class Controllertest {
+public class ControllerTest{
     
     @Test
     void testTransaction() {
