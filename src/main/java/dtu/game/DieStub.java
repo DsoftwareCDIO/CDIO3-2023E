698dc3c7014913158c58798dddcd132ff649595b
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2023-11-22 19:58:26 +0100

    Changed DieStub to hit 3 and build#6

--- src/main/java/dtu/game/DieStub.java
+++ src/main/java/dtu/game/DieStub.java
@@ -5,7 +5,7 @@ import dtu.UIController;
 //6 sided die to roll on turn, same old same old
 public class DieStub {
     public int roll(){
-        int roll = 1;
+        int roll = 3;
         UIController.dieRollResult(roll, MonopolyJunior.currentPlayer);
         return roll;
     }
